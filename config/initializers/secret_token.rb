bebb3b23995f3346dc1f3afbee2af9ef22ee6f86
Casey West <casey@geeknest.com>
2012-10-15 19:41:31 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrooklynbetaOnInstagram::Application.config.secret_token = 'b753a729fb23aaa04e90109c4ce93cc5e7b9126ee86044e593aa0b218e367c990036d157bb4f665edc1e0707920c1feac11721bcfd8d87d87b98efc05dd59e19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BrooklynbetaOnInstagram::Application.config.session_store :cookie_store, key: '_brooklynbeta_on_instagram_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BrooklynbetaOnInstagram::Application.config.session_store :active_record_store
